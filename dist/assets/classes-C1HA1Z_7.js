e2f5e6011dbf1bacffd2245720fdb91a523fcdf7
Sahil Tolani <Sahiltolani86@gmail.com>
2026-03-02 03:11:33 +0530

    Fix netlify deployment by removing node_modules from git

--- dist/assets/classes-C1HA1Z_7.js
+++
@@ -1,1 +0,0 @@
-import"./main-CeP03lpp.js";import{r as e}from"./data-fetch-B3Qp8oFB.js";document.addEventListener("DOMContentLoaded",()=>{e("full-schedule")});
--- dist/assets/trainers-BHjzwHrb.js
+++
@@ -1,1 +0,0 @@
-import"./main-CeP03lpp.js";import{b as r}from"./data-fetch-B3Qp8oFB.js";document.addEventListener("DOMContentLoaded",()=>{r("trainer-roster")});
